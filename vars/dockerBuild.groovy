0bd0bc6d01a84b11cce4984fbc15d39e8ff13887
hemanthkumar-git <167083933+hemanthkumar-git@users.noreply.github.com>
2024-04-15 17:25:50 +0530

    Update dockerBuild.groovy
    hemath docker id updated

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -3,7 +3,7 @@ def call(String project, String hubUser) {
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}"
     sh "docker tag ${hubUser}/${project} ${hubUser}/${project}:latest"
     withCredentials([usernamePassword(
-            credentialsId: "docker-ramya-cred",
+            credentialsId: "hemanth_docker_id",
             usernameVariable: "USER",
             passwordVariable: "PASS"
     )]) {
